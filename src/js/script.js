0351ec6db0bb48f35d8557da0f24cf95756beaef
Gabriela Cichowicz <gabriela.cichowicz@gmail.com>
2020-04-22 18:51:57 +0200

    Accordion completed

--- src/js/script.js
+++ src/js/script.js
@@ -59,6 +59,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
 
@@ -78,7 +79,40 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
-  }
+
+    initAccordion(){
+        const thisProduct = this;
+
+        /* find the clickable trigger (the element that should react to clicking) */
+        const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+        /* START: click event listener to trigger */
+        accordionTrigger.addEventListener('click', function() {
+
+          /* prevent default action for event */
+          event.preventDefault();
+
+          /* toggle active class on element of thisProduct */
+          thisProduct.element.classList.toggle('active');
+
+          /* find all active products */
+          const activeProducts = document.querySelectorAll('article.active');
+
+          /* START LOOP: for each active product */
+          for (let activeProduct of activeProducts) {
+            /* START: if the active product isn't the element of thisProduct */
+            if (activeProduct != thisProduct.element) {
+              /* remove class active for the active product */
+              activeProduct.classList.remove('active');
+            /* END: if the active product isn't the element of thisProduct */
+            }
+          /* END LOOP: for each active product */
+          }
+        /* END: click event listener to trigger */
+      });
+    }
+
+} // ten nawias powinien zamykać klasę Product
 
   const app = {
     initMenu: function(){
